8970cca1eeb38eabf100b22aadc1a43fe1367ad5
samarthhatte <109466822+samarthhatte@users.noreply.github.com>
2025-11-01 21:30:28 +0530

    React UI fix + issue solved

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+<div>
+  <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+    <img src={viteLogo} className="logo" alt="Vite logo" />
+  </a>
+  <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </a>
+</div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
